60acce5b0406f518f058c98ba78980c6f6f23b95
VPerewalow <127751073+VPerewalow@users.noreply.github.com>
2023-03-26 23:53:12 +0300

    Add files via upload

---
+++ src/part1.java
@@ -0,0 +1,7 @@
+package wh;
+
+public class part1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/part3.java
@@ -0,0 +1,10 @@
+package wh;
+
+import com.sun.source.util.SourcePositions;
+
+public class part3 {
+    public static void main(String[] args) {
+        System.out.println("Hello Belarus!");
+        System.out.println("I like Java!");
+    }
+}
