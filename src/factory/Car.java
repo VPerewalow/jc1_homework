453e3dee8eca84fe1d2db486c588a1f8ce807214
Vladimir Perevalov <perewalow@gmail.com>
2023-04-20 20:56:36 +0300

    -

---
+++ src/factory/Car.java
@@ -0,0 +1,23 @@
+package factory;
+
+public abstract class Car {
+    public Car(CarType model) {
+        this.model = model;
+        arrangeParts();
+    }
+
+    private void arrangeParts() {
+    }
+
+    protected abstract void construct();
+
+    private CarType model;
+
+    public CarType getModel() {
+        return model;
+    }
+
+    public void setModel(CarType model) {
+        this.model = model;
+    }
+}
